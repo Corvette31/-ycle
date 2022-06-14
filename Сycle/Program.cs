f62b7e3b4498ea7a12c59d8add3ca323f1df258b
Corvette31 <corvette.ss@yandex.ru>
2022-06-15 02:31:09 +0300

    Добавьте файлы проекта.

---
+++ Сycle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Сycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int usetInput;
+
+            Console.Write("Введите кол-во повторов сообщения: ");
+            usetInput = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= usetInput; i++)
+            {
+                Console.WriteLine($"Это сообщение вывелось {i} раз");
+            }
+        }
+    }
+}
