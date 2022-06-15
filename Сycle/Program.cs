4311c80fc5aae8918cc965e4935ba24287b17681
Corvette31 <corvette.ss@yandex.ru>
2022-06-15 15:44:05 +0300

    исправил в соответсвии с замечаниями

--- Сycle/Program.cs
+++ Сycle/Program.cs
@@ -7,12 +7,16 @@ namespace Сycle
         static void Main(string[] args)
         {
             int usetInput;
+            string repeatableMessage;
 
+            Console.Write("Введите сообщение которое будет вывыдоться: ");
+            repeatableMessage = Console.ReadLine();
             Console.Write("Введите кол-во повторов сообщения: ");
             usetInput = Convert.ToInt32(Console.ReadLine());
+
             for (int i = 1; i <= usetInput; i++)
             {
-                Console.WriteLine($"Это сообщение вывелось {i} раз");
+                Console.WriteLine(repeatableMessage);
             }
         }
     }
